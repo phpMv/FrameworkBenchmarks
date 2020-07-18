698d244da3681157e4f38c77054786b4f3c5654f
jcheron <myaddressmail@gmail.com>
2020-07-18 02:17:19 +0200

    Update workerman-config.php

--- frameworks/PHP/ubiquity/.ubiquity/workerman-config.php
+++ frameworks/PHP/ubiquity/.ubiquity/workerman-config.php
@@ -2,8 +2,10 @@
 return[
 	'host' => '0.0.0.0',
 	'port' => 8080,
-	'context' => [],
-	'max_request'=>10000000000,
-	'transport'=>'tcp',
-	'max_package_size'=> 10*1024*1024
+	'socket'=>[
+		'context' => [],
+		'max_request'=>10000000000,
+		'transport'=>'tcp',
+		'max_package_size'=> 10*1024*1024
+	]
 ];
