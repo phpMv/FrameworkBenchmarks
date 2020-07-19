ee6eec81b425f28212b0d3931ef98b42a4fbf478
jcheron <myaddressmail@gmail.com>
2020-07-19 17:35:14 +0200

    Update Fortunes_.php

--- frameworks/PHP/ubiquity/app/controllers/Fortunes_.php
+++ frameworks/PHP/ubiquity/app/controllers/Fortunes_.php
@@ -12,13 +12,13 @@ class Fortunes_ extends \Ubiquity\controllers\SimpleViewAsyncController {
 	public function index() {
 		$fortunes = \Ubiquity\orm\DAO::executePrepared('fortune');
 		$fortunes[] = new Fortune(0, 'Additional fortune added at request time.');
-		\usort($fortunes, 'self::compareTo');
+		\usort($fortunes, 'self::compareToX');
 		$this->loadView('Fortunes/index.php', [
 			'fortunes' => $fortunes
 		]);
 	}
 
-	private static function compareTo(Fortune $left, Fortune $right):int{
+	private static function compareToX(Fortune $left, Fortune $right):int{
 		return $left->message <=> $right->message;
 	}
 }
