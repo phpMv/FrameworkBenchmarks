c8886f7fa23e1c0a55792614181d74a97f52540b
jcheron <myaddressmail@gmail.com>
2020-07-23 11:52:46 +0200

    memcached conf

--- frameworks/PHP/ubiquity/run-fpm-memcached.sh
+++ frameworks/PHP/ubiquity/run-fpm-memcached.sh
@@ -1,20 +1,9 @@
 #!/bin/bash
+cp /ubiquity/deploy/memcached/memcached.conf /etc/memcached.conf 
 
-/etc/init.d/memcached stop
-cp /etc/memcached.conf /etc/memcached_11212.conf
-sed -i "s|11211|11212|g" /etc/memcached_11212.conf
+/etc/init.d/memcached start
 
-cp /lib/systemd/system/memcached.service /lib/systemd/system/memcached2.service
-sed -i "s|/etc/memcached.conf|/etc/memcached_11212.conf|g" /lib/systemd/system/memcached2.service
-
-cp /etc/init.d/memcached /etc/init.d/memcached2
-chmod 700 /etc/init.d/memcached2
-
-
-service memcached start -u memcache
-service memcached2 start -u memcache
-
-#/etc/init.d/memcached start -d -p 11211 -m 256 -u memcache
+/etc/init.d/memcached status
 
 ps aux | grep memcached
 
