7ce7de59314985efc39e14c950f7a6c419e282a4
jcheron <myaddressmail@gmail.com>
2020-07-23 11:41:15 +0200

    Update run-fpm-memcached.sh

--- frameworks/PHP/ubiquity/run-fpm-memcached.sh
+++ frameworks/PHP/ubiquity/run-fpm-memcached.sh
@@ -7,6 +7,10 @@ sed -i "s|11211|11212|g" /etc/memcached_11212.conf
 cp /lib/systemd/system/memcached.service /lib/systemd/system/memcached2.service
 sed -i "s|/etc/memcached.conf|/etc/memcached_11212.conf|g" /lib/systemd/system/memcached2.service
 
+cp /etc/init.d/memcached /etc/init.d/memcached2
+chmod 700 /etc/init.d/memcached2
+
+
 service memcached start -u memcache
 service memcached2 start -u memcache
 
