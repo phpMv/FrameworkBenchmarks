a6e754a5c260d260025e5bb8d7fc92af01a3ed65
jcheron <myaddressmail@gmail.com>
2020-07-28 14:43:18 +0200

    add db raw

---
+++ frameworks/PHP/ubiquity/app/controllers/DbPgRaw.php
@@ -0,0 +1,48 @@
+<?php
+namespace controllers;
+
+use controllers\utils\DbTrait;
+use controllers\utils\RawDb;
+/**
+ * Bench controller.
+ */
+class DbPgRaw extends \Ubiquity\controllers\Controller {
+	use DbTrait;
+
+	public function __construct() {
+	}
+
+	public function initialize() {
+		\Ubiquity\utils\http\UResponse::setContentType('application/json');
+	}
+
+	public function index() {
+		RawDb::$worlds->execute([\mt_rand(1, 10000)]);
+		echo \json_encode(RawDb::$worlds->fetch());
+	}
+
+	public function query($queries = 1) {
+		$worlds = [];
+		$count=self::getCount($queries);
+		for ($i = 0; $i < $count; ++ $i) {
+			RawDb::$worlds->execute([\mt_rand(1, 10000)]);
+			$worlds[] = RawDb::$worlds->fetch();
+		}
+		echo \json_encode($worlds);
+	}
+
+	public function update($queries = 1) {
+		$worlds = [];
+		$count=self::getCount($queries);
+
+		for ($i = 0; $i < $count; ++ $i) {
+			$id=\mt_rand(1,10000);
+			RawDb::$random->execute([$id]);
+			$row = ['id' => $id, 'randomNumber' => RawDb::$random->fetchColumn()];
+			$row['randomNumber'] = \mt_rand(1, 10000);
+			$worlds[] = $row;
+		}
+		RawDb::update($worlds);
+		echo \json_encode($worlds);
+	}
+}
--- frameworks/PHP/ubiquity/app/controllers/utils/RawDb.php
+++ frameworks/PHP/ubiquity/app/controllers/utils/RawDb.php
@@ -2,12 +2,35 @@
 namespace controllers\utils;
 
 class RawDb {
+	private static $updates;
 	private static $db;
 	public static $fortunes;
+	public static $worlds;
+	public static $random;
 
-	public static function prepare($config){
+	public static function prepare(array $config){
 		self::$db=\Ubiquity\db\Database::start('raw',$config);
 		self::$fortunes=self::$db->prepareStatement('SELECT id,message FROM Fortune');
+		self::$worlds=self::$db->prepareStatement('SELECT id,randomNumber FROM World WHERE id=?');
+		self::$random=self::$db->prepareStatement('SELECT randomNumber FROM World WHERE id=?');
+		self::prepareUpdates([1,5,10,15,20]);
+	}
+
+	private static function prepareUpdate(int $count){
+			$sql = 'UPDATE world SET randomNumber = CASE id'. \str_repeat(' WHEN ? THEN ? ', $count) .'END WHERE id IN (' . \str_repeat ( '?,', $count - 1 ) . '?)';
+			self::$updates[$count] = self::$db->prepareStatement($sql);
+		}
+	}
+
+	public static function update(array $worlds){
+		$count = \count($worlds);
+		self::$updates[$count]??=self::prepareUpdate($count);
+		$values = [];
+		$keys = [];
+		foreach ($worlds as $world) {
+			$values[] = $keys[] = $world['id'];
+			$values[] = $world['randomNumber'];
+		}
+		self::$updates[$count]->execute([...$values, ...$keys]);
 	}
 }
-
