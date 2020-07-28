649e0c027ae05135fbe3421238c0ea27f952021d
jcheron <myaddressmail@gmail.com>
2020-07-28 03:17:34 +0200

    fix typo

--- frameworks/PHP/ubiquity/app/controllers/FortunesRaw.php
+++ frameworks/PHP/ubiquity/app/controllers/FortunesRaw.php
@@ -7,8 +7,8 @@ class FortunesRaw extends \Ubiquity\controllers\SimpleViewAsyncController {
 
 
 	public function index() {
-		RawDb::$fortune->execute();
-		$fortunes= RawDb::$fortune->fetchAll(\PDO::FETCH_KEY_PAIR);
+		RawDb::$fortunes->execute();
+		$fortunes= RawDb::$fortunes->fetchAll(\PDO::FETCH_KEY_PAIR);
 		$fortunes[0] = 'Additional fortune added at request time.';
 		\asort($fortunes);
 		$this->loadView('Fortunes/raw.php', [
