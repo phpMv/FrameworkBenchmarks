202e5329a1bbd686da64eba64bd286b5f38f5ac8
jcheron <myaddressmail@gmail.com>
2020-07-28 20:27:44 +0200

    remove random

--- frameworks/PHP/ubiquity/app/controllers/utils/RawDb.php
+++ frameworks/PHP/ubiquity/app/controllers/utils/RawDb.php
@@ -17,7 +17,7 @@ class RawDb {
 		self::$db = \Ubiquity\db\Database::start('raw', $config);
 		self::$fortunes = self::$db->prepareStatement('SELECT id,message FROM Fortune');
 		self::$worlds = self::$db->prepareStatement('SELECT id,randomNumber FROM World WHERE id=?::INTEGER');
-		self::$random = self::$db->prepareStatement('SELECT randomNumber FROM World WHERE id=?::INTEGER');
+		// self::$random = self::$db->prepareStatement('SELECT randomNumber FROM World WHERE id=?::INTEGER');
 	}
 
 	private static function prepareUpdate(int $count) {
