eb4cd1d14aaf34c04659270c91ab313734e4ed2b
jcheron <myaddressmail@gmail.com>
2020-07-28 15:34:38 +0200

    Update DbPgRaw.php

--- frameworks/PHP/ubiquity/app/controllers/DbPgRaw.php
+++ frameworks/PHP/ubiquity/app/controllers/DbPgRaw.php
@@ -3,29 +3,33 @@ namespace controllers;
 
 use controllers\utils\DbTrait;
 use controllers\utils\RawDb;
+
 /**
  * Bench controller.
  */
 class DbPgRaw extends \Ubiquity\controllers\Controller {
 	use DbTrait;
 
-	public function __construct() {
-	}
+	public function __construct() {}
 
 	public function initialize() {
 		\Ubiquity\utils\http\UResponse::setContentType('application/json');
 	}
 
 	public function index() {
-		RawDb::$worlds->execute([\mt_rand(1, 10000)]);
+		RawDb::$worlds->execute([
+			\mt_rand(1, 10000)
+		]);
 		echo \json_encode(RawDb::$worlds->fetch());
 	}
 
 	public function query($queries = 1) {
 		$worlds = [];
-		$count=self::getCount($queries);
-		for ($i = 0; $i < $count; ++ $i) {
-			RawDb::$worlds->execute([\mt_rand(1, 10000)]);
+		$count = self::getCount($queries);
+		while ($count --) {
+			RawDb::$worlds->execute([
+				\mt_rand(1, 10000)
+			]);
 			$worlds[] = RawDb::$worlds->fetch();
 		}
 		echo \json_encode($worlds);
@@ -33,12 +37,17 @@ class DbPgRaw extends \Ubiquity\controllers\Controller {
 
 	public function update($queries = 1) {
 		$worlds = [];
-		$count=self::getCount($queries);
+		$count = self::getCount($queries);
 
 		for ($i = 0; $i < $count; ++ $i) {
-			$id=\mt_rand(1,10000);
-			RawDb::$random->execute([$id]);
-			$row = ['id' => $id, 'randomNumber' => RawDb::$random->fetchColumn()];
+			$id = \mt_rand(1, 10000);
+			RawDb::$random->execute([
+				$id
+			]);
+			$row = [
+				'id' => $id,
+				'randomNumber' => RawDb::$random->fetchColumn()
+			];
 			$row['randomNumber'] = \mt_rand(1, 10000);
 			$worlds[] = $row;
 		}
