69cb7536949d4d1a9e16a891e15b778087146418
jcheron <myaddressmail@gmail.com>
2020-07-28 19:57:41 +0200

    Update workerServices.php

--- frameworks/PHP/ubiquity/deploy/worker/workerServices.php
+++ frameworks/PHP/ubiquity/deploy/worker/workerServices.php
@@ -1,12 +1,12 @@
 <?php
-
 \Ubiquity\cache\CacheManager::startProd($config);
 
+\Ubiquity\cache\CacheManager::warmUpControllers([
+	'controllers\\DbPgRaw',
+	'controllers\\FortunesRaw'
+]);
+
 $workerServer->onWorkerStart = function () use ($config) {
-	\Ubiquity\cache\CacheManager::warmUpControllers([
-		'controllers\\DbPgRaw',
-		'controllers\\FortunesRaw'
-	]);
 	\controllers\utils\RawDb::prepare($config);
 };
 
