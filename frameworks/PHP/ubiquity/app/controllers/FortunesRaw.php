94ac792df4cb4c45a3c9320c132e9175f59ca427
jcheron <myaddressmail@gmail.com>
2020-07-29 15:57:20 +0200

    Update FortunesRaw.php

--- frameworks/PHP/ubiquity/app/controllers/FortunesRaw.php
+++ frameworks/PHP/ubiquity/app/controllers/FortunesRaw.php
@@ -6,7 +6,7 @@ class FortunesRaw extends \Ubiquity\controllers\SimpleViewAsyncController {
 	protected static $statement;
 
 	public static function warmup(\Ubiquity\db\Database $db) {
-		$db->prepareStatement('SELECT id,message FROM Fortune');
+		self::$statement = $db->prepareStatement('SELECT id,message FROM Fortune');
 	}
 
 	public function index() {
@@ -14,9 +14,7 @@ class FortunesRaw extends \Ubiquity\controllers\SimpleViewAsyncController {
 		$fortunes = self::$statement->fetchAll(\PDO::FETCH_KEY_PAIR);
 		$fortunes[0] = 'Additional fortune added at request time.';
 		\asort($fortunes);
-		$this->loadView('Fortunes/raw.php', [
-			'fortunes' => $fortunes
-		]);
+		$this->loadView('Fortunes/raw.php', ['fortunes' => $fortunes]);
 	}
 }
 
