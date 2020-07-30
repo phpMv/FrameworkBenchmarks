d03f573f25fe7243dbe42b8f94c1fb020a80b96d
jcheron <myaddressmail@gmail.com>
2020-07-30 13:00:46 +0200

    Update workerman-config.php

--- frameworks/PHP/ubiquity/.ubiquity/workerman-config.php
+++ frameworks/PHP/ubiquity/.ubiquity/workerman-config.php
@@ -1,10 +1,13 @@
 <?php
-return[
+return [
 	'host' => '0.0.0.0',
 	'port' => 8080,
-	'options'=>['socket'=>[
-		'max_request'=>10000000000,
-		'transport'=>'tcp',
-		'max_package_size'=> 10*1024*1024
-	]]
+	'options' => [
+		'socket' => [
+			'max_request' => 10000000000,
+			'transport' => 'tcp',
+			'max_package_size' => 10 * 1024 * 1024
+		],
+		'debug' => false
+	]
 ];
