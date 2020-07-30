4f19c17a00c074257ee714fc3bea93e9c8ed4fca
jcheron <myaddressmail@gmail.com>
2020-07-30 11:36:41 +0200

    Update DbPgRaw.php

--- frameworks/PHP/ubiquity/app/controllers/DbPgRaw.php
+++ frameworks/PHP/ubiquity/app/controllers/DbPgRaw.php
@@ -11,13 +11,18 @@ class DbPgRaw extends \Ubiquity\controllers\Controller {
 
 	protected static $statement;
 
-	protected static $updates;
+	protected static $uStatements;
 
 	/**
 	 *
 	 * @var \Ubiquity\db\Database
 	 */
 	protected static $db;
+
+	private static function prepareUpdate(int $count) {
+		$sql = 'UPDATE World SET randomNumber = CASE id' . \str_repeat(' WHEN ?::INTEGER THEN ?::INTEGER ', $count) . 'END WHERE id IN (' . \str_repeat('?::INTEGER,', $count - 1) . '?::INTEGER)';
+		return self::$uStatements[$count] = self::$db->prepareStatement($sql);
+	}
 
 	public function __construct() {}
 
@@ -31,17 +36,20 @@ class DbPgRaw extends \Ubiquity\controllers\Controller {
 	}
 
 	public function index() {
-		self::$statement->execute([\mt_rand(1, 10000)]);
+		self::$statement->execute([
+			\mt_rand(1, 10000)
+		]);
 		echo \json_encode(self::$statement->fetch());
 	}
 
 	public function query($queries = 1) {
 		$worlds = [];
 		$count = $this->getCount($queries);
-		$st = self::$statement;
 		while ($count --) {
-			$st->execute([\mt_rand(1, 10000)]);
-			$worlds[] = $st->fetch();
+			self::$statement->execute([
+				\mt_rand(1, 10000)
+			]);
+			$worlds[] = self::$statement->fetch();
 		}
 		echo \json_encode($worlds);
 	}
@@ -50,21 +58,20 @@ class DbPgRaw extends \Ubiquity\controllers\Controller {
 		$worlds = [];
 		$keys = $values = [];
 		$count = $this->getCount($queries);
-		$st = self::$statement;
 		for ($i = 0; $i < $count; ++ $i) {
 			$values[] = $keys[] = $id = \mt_rand(1, 10000);
-			$st->execute([$id]);
-			$row = $st->fetch();
+			self::$statement->execute([
+				$id
+			]);
+			$row = self::$statement->fetch();
 
 			$values[] = $row['randomNumber'] = \mt_rand(1, 10000);
 			$worlds[] = $row;
 		}
-		(self::$updates[$count] ?? self::prepareUpdate($count))->execute([...$values,...$keys]);
+		(self::$uStatements[$count] ?? self::prepareUpdate($count))->execute([
+			...$values,
+			...$keys
+		]);
 		echo \json_encode($worlds);
-	}
-
-	private static function prepareUpdate(int $count) {
-		$sql = 'UPDATE World SET randomNumber = CASE id' . \str_repeat(' WHEN ?::INTEGER THEN ?::INTEGER ', $count) . 'END WHERE id IN (' . \str_repeat('?::INTEGER,', $count - 1) . '?::INTEGER)';
-		return self::$updates[$count] = self::$db->prepareStatement($sql);
 	}
 }
