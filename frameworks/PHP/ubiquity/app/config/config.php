998e60f68feb3d0878c14b2107c831555553bbf1
jcheron <myaddressmail@gmail.com>
2020-07-31 14:16:03 +0200

    emulate prepares

--- frameworks/PHP/ubiquity/app/config/config.php
+++ frameworks/PHP/ubiquity/app/config/config.php
@@ -23,8 +23,7 @@ return array(
 			"user" => "benchmarkdbuser", // benchmarkdbuser
 			"password" => "benchmarkdbpass", // benchmarkdbpass
 			"options" => [
-				\PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
-				\PDO::ATTR_EMULATE_PREPARES => false
+				\PDO::ATTR_EMULATE_PREPARES => true
 			],
 			"cache" => false
 		],
