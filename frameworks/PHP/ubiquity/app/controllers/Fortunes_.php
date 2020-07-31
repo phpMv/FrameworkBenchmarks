6ea0a9b2d5b64853754b5aebc577064566847255
jcheron <myaddressmail@gmail.com>
2020-07-31 14:27:42 +0200

    remove compare

--- frameworks/PHP/ubiquity/app/controllers/Fortunes_.php
+++ frameworks/PHP/ubiquity/app/controllers/Fortunes_.php
@@ -2,16 +2,17 @@
 namespace controllers;
 
 use models\Fortune;
-use controllers\utils\FortunesTrait;
 use controllers\utils\FortunesAsyncTrait;
 
 class Fortunes_ extends \Ubiquity\controllers\SimpleViewAsyncController {
-	use FortunesTrait,FortunesAsyncTrait;
+	use FortunesAsyncTrait;
 
 	public function index() {
 		$fortunes = self::$pDao->execute();
 		$fortunes[] = new Fortune(0, 'Additional fortune added at request time.');
-		\usort($fortunes, 'self::compare');
+		\usort($fortunes, function ($left, $right) {
+			return $left->message <=> $right->message;
+		});
 		$this->loadView('Fortunes/index.php', [
 			'fortunes' => $fortunes
 		]);
