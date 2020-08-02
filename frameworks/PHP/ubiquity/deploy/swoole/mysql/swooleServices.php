e05b91919c1926902a9fbe10c1ac2ff2d755cb25
jcheron <myaddressmail@gmail.com>
2020-08-02 03:32:29 +0200

    Update swooleServices.php

--- frameworks/PHP/ubiquity/deploy/swoole/mysql/swooleServices.php
+++ frameworks/PHP/ubiquity/deploy/swoole/mysql/swooleServices.php
@@ -1,12 +1,12 @@
 <?php
 \Ubiquity\cache\CacheManager::startProd($config);
 \Ubiquity\orm\DAO::setModelsDatabases([
-	'models\\World' => 'default'
+	'models\\World' => 'mysql'
 ]);
 \Ubiquity\cache\CacheManager::warmUpControllers([
 	\controllers\DbMy::class
 ]);
 $swooleServer->on('workerStart', function ($srv) use (&$config) {
-	\Ubiquity\orm\DAO::startDatabase($config, 'default');
+	\Ubiquity\orm\DAO::startDatabase($config, 'mysql');
 	\controllers\DbMy::warmup();
 });
