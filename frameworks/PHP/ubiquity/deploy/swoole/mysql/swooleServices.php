28ba6e8b3cf0dfa9a6f92f02f9c7baa0ad4b423e
jcheron <myaddressmail@gmail.com>
2020-08-02 12:21:05 +0200

    remove mysql-async

--- frameworks/PHP/ubiquity/deploy/swoole/mysql/swooleServices.php
+++ frameworks/PHP/ubiquity/deploy/swoole/mysql/swooleServices.php
@@ -4,9 +4,11 @@
 	'models\\World' => 'mysql'
 ]);
 \Ubiquity\cache\CacheManager::warmUpControllers([
-	\controllers\DbMy::class
+	\controllers\DbMy::class,
+	\controllers\Fortunes_::class
 ]);
 $swooleServer->on('workerStart', function ($srv) use (&$config) {
 	\Ubiquity\orm\DAO::startDatabase($config, 'mysql');
 	\controllers\DbMy::warmup();
+	\controllers\Fortunes_::warmup();
 });
