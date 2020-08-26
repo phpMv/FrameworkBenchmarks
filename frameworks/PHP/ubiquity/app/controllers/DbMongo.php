820a5f3b711ccfacc7f59f18105b035c8eaed44c
jcheron <myaddressmail@gmail.com>
2020-08-26 11:50:05 +0200

    Update DbMongo.php

--- frameworks/PHP/ubiquity/app/controllers/DbMongo.php
+++ frameworks/PHP/ubiquity/app/controllers/DbMongo.php
@@ -36,32 +36,19 @@ class DbMongo extends \Ubiquity\controllers\Controller {
 	public function update($queries = 1) {
 		$worlds = [];
 		$count = $this->getCount($queries);
-		$ids = $this->getUniqueRandomNumbers($count);
-		$bId = DAONosql::startBulk(World::class);
-		foreach ($ids as $id) {
+		while ($count --) {
 			$world = self::$pDao->execute([
-				'id' => $id
+				'id' => \mt_rand(1, 10000)
 			]);
 			do {
 				$nRn = \mt_rand(1, 10000);
 			} while ($world->randomNumber === $nRn);
 			$world->randomNumber = $nRn;
-			DAONosql::toUpdate($world, $bId);
+			DAONosql::toUpdate($world);
 			$worlds[] = $world->_rest;
 		}
-		DAONosql::flush($bId, true);
+		DAONosql::flush(World::class);
 
 		echo \json_encode($worlds);
-	}
-
-	private function getUniqueRandomNumbers($count) {
-		$res = [];
-		do {
-			$res[\mt_rand(1, 10000)] = 1;
-		} while (\count($res) < $count);
-
-		\ksort($res);
-
-		return \array_keys($res);
 	}
 }
